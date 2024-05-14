c375a5194307cb175af444cab5842c070bffb304
csini <csini@gmx.com>
2024-05-14 19:16:57 +0200

    ISSUE-4: aggreate batched

--- src/main/java/hu/exercise/spring/kafka/cogroup/CustomProductPairAggregator.java
+++ src/main/java/hu/exercise/spring/kafka/cogroup/CustomProductPairAggregator.java
@@ -3,8 +3,19 @@ package hu.exercise.spring.kafka.cogroup;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.processor.PunctuationType;
@@ -89,7 +100,7 @@ public class CustomProductPairAggregator implements Processor<String, ProductEve
 
 		store.put(rec.key(), oldValue);
 	}
-
+	
 	private void flushStore() {
 		AtomicInteger counter = new AtomicInteger();
 		try (final KeyValueIterator<String, ProductPair> it = store.all()) {
@@ -109,6 +120,93 @@ public class CustomProductPairAggregator implements Processor<String, ProductEve
 					toBeForwarded, new Date().getTime()));
 			flushCounter++;
 
+		}
+	}
+
+	// batchSize = 150, processCounter not working!
+	private void flushStoreBatched(int batchSize) {
+		try (final KeyValueIterator<String, ProductPair> it = store.all()) {
+
+			Stream<KeyValue<String, ProductPair>> stream = getStreamFromIterator(it);
+			partitionStream(stream, batchSize).forEach(nextList -> {
+				AtomicInteger counter = new AtomicInteger();
+				ProductRollup toBeForwarded = new ProductRollup(environment.getRequestid().toString(), flushCounter,
+						processCounter);
+				if (LOGGER.isDebugEnabled()) {
+					LOGGER.debug("nextList: " + nextList);
+				}
+				nextList.stream().forEach(next -> {
+					toBeForwarded.getPairList().add(next.value);
+					counter.incrementAndGet();
+					store.delete(next.key);
+				});
+				LOGGER.warn("flushed (" + flushCounter + "): " + counter);
+				context.forward(new Record<String, ProductRollup>(environment.getRequestid() + "-" + flushCounter,
+						toBeForwarded, new Date().getTime()));
+				flushCounter++;
+			});
+
+		}
+	}
+
+	// https://www.geeksforgeeks.org/convert-an-iterator-to-stream-in-java/
+	// Function to get the Stream
+	public static <T> Stream<T> getStreamFromIterator(Iterator<T> iterator) {
+
+		// Convert the iterator to Spliterator
+		Spliterator<T> spliterator = Spliterators.spliteratorUnknownSize(iterator, 0);
+
+		// Get a Sequential Stream from spliterator
+		return StreamSupport.stream(spliterator, false);
+	}
+
+	static <T> List<List<T>> partitionStream(Stream<T> source, int batchSize) {
+		return source.collect(partitionBySize(batchSize, Collectors.toList()));
+	}
+
+	// https://www.baeldung.com/java-partition-stream
+	static <T, A, R> Collector<T, ?, R> partitionBySize(int batchSize, Collector<List<T>, A, R> downstream) {
+		Supplier<Accumulator<T, A>> supplier = () -> new Accumulator<>(batchSize, downstream.supplier().get(),
+				downstream.accumulator()::accept);
+
+		BiConsumer<Accumulator<T, A>, T> accumulator = (acc, value) -> acc.add(value);
+
+		BinaryOperator<Accumulator<T, A>> combiner = (acc1, acc2) -> acc1.combine(acc2, downstream.combiner());
+
+		Function<Accumulator<T, A>, R> finisher = acc -> {
+			if (!acc.values.isEmpty()) {
+				downstream.accumulator().accept(acc.downstreamAccumulator, acc.values);
+			}
+			return downstream.finisher().apply(acc.downstreamAccumulator);
+		};
+
+		return Collector.of(supplier, accumulator, combiner, finisher, Collector.Characteristics.UNORDERED);
+	}
+
+	static class Accumulator<T, A> {
+		private final List<T> values = new ArrayList<>();
+		private final int batchSize;
+		private A downstreamAccumulator;
+		private final BiConsumer<A, List<T>> batchFullListener;
+
+		Accumulator(int batchSize, A accumulator, BiConsumer<A, List<T>> onBatchFull) {
+			this.batchSize = batchSize;
+			this.downstreamAccumulator = accumulator;
+			this.batchFullListener = onBatchFull;
+		}
+
+		void add(T value) {
+			values.add(value);
+			if (values.size() == batchSize) {
+				batchFullListener.accept(downstreamAccumulator, new ArrayList<>(values));
+				values.clear();
+			}
+		}
+
+		Accumulator<T, A> combine(Accumulator<T, A> other, BinaryOperator<A> accumulatorCombiner) {
+			this.downstreamAccumulator = accumulatorCombiner.apply(downstreamAccumulator, other.downstreamAccumulator);
+			other.values.forEach(this::add);
+			return this;
 		}
 	}
 
